14cf287870e3ace011ff3dcd38184c339fec8f5d
gurpreet singh <gurpreetlambton21@gmail.com>
2023-07-27 16:40:29 -0400

    footer  commit

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -1,58 +1,52 @@
 import React from 'react'
-import {BiLogoLinkedin,BiLogoInstagram,BiLogoFacebookSquare } from 'react-icons/bi'
-import {FaTiktok} from 'react-icons/fa'
+import { BiLogoLinkedin, BiLogoInstagram, BiLogoFacebookSquare } from 'react-icons/bi'
+import { FaTiktok } from 'react-icons/fa'
+import Link from 'next/link'
 
-const data =[
+const data = [
   {
-    icon:  <BiLogoLinkedin />
+    icon: <BiLogoLinkedin />
   },
   {
-    icon:   <BiLogoInstagram />
+    icon: <BiLogoInstagram />
   },
   {
-    icon:  <FaTiktok/>
+    icon: <FaTiktok />
   },
   {
-    icon:       <BiLogoFacebookSquare />
+    icon: <BiLogoFacebookSquare />
   }
 ]
 
 const Footer = () => {
   return (
     <footer className="bg-slate-950 flex mt-4 flex-col justify-center items-center dark:bg-gray-900">
-    <div className="container flex flex-col items-center justify-between px-6 py-8 mx-auto lg:flex-row">
-     
-    <p className="mt-6 text-base text-white lg:mt-0 hover:bg-white  hover:text-gray-900 bg-gray-900 rounded-lg  p-2 ">
-        © Copyright 2023 webGtario.{" "}
-      </p>
-      <a href="#">
-      <img className='w-12' src='/11.png'/>
-      </a>
-     
-    
-      <div className="flex flex-wrap items-center justify-center gap-4 mt-6 lg:gap-6 lg:mt-0">
-      
-   <Link className="text-base text-white transition-colors duration-300  hover:bg-white  hover:text-gray-900 bg-gray-900 rounded-lg  p-2" href="/">HOME</Link>
-   <Link className="text-base text-white transition-colors duration-300  hover:bg-white  hover:text-gray-900 bg-gray-900 rounded-lg  p-2" href="/service">SERVICE</Link>
-   <Link className="text-base text-white transition-colors duration-300  hover:bg-white  hover:text-gray-900 bg-gray-900 rounded-lg  p-2" href="/about">ABOUT</Link>
+      <div className="container flex flex-col items-center justify-between px-6 py-8 mx-auto lg:flex-row">
+
+        <p className="mt-6 text-base text-white lg:mt-0 hover:bg-white  hover:text-gray-900 bg-gray-900 rounded-lg  p-2 ">
+          © Copyright 2023 webGtario.{" "}
+        </p>
+        <Link className="text-base text-white transition-colors duration-300  hover:bg-white  hover:text-gray-900 bg-gray-900 rounded-lg  p-2" href="/">WEBGTARIO</Link>
+
+
+        <div className="flex flex-wrap items-center justify-center gap-4 mt-6 lg:gap-6 lg:mt-0">
+
+          <Link className="text-base text-white transition-colors duration-300  hover:bg-white  hover:text-gray-900 bg-gray-900 rounded-lg  p-2" href="/">HOME</Link>
+          <Link className="text-base text-white transition-colors duration-300  hover:bg-white  hover:text-gray-900 bg-gray-900 rounded-lg  p-2" href="/service">SERVICE</Link>
+          <Link className="text-base text-white transition-colors duration-300  hover:bg-white  hover:text-gray-900 bg-gray-900 rounded-lg  p-2" href="/about">ABOUT</Link>
+        </div>
       </div>
-    </div>
 
-    <div className='text-white text-4xl flex items-center justify-center gap-5 pb-2  '>
-   
-    {
-      data.map((ele,index)=>(
-        <p  key={index} className='hover:bg-white  hover:text-gray-900 bg-gray-900 rounded-lg  p-1 '>{ele.icon}</p>
-      ))
-    }
-     
-      
+      <div className='text-white text-4xl flex items-center justify-center gap-5 pb-2  '>
 
+        {
+          data.map((ele, index) => (
+            <p key={index} className='hover:bg-white  hover:text-gray-900 bg-gray-900 rounded-lg  p-1 '>{ele.icon}</p>
+          ))
+        }
       </div>
-   
+    </footer>
 
-  </footer>
-  
   )
 }
 
--- src/app/Components/NavBar.jsx
+++ src/app/Components/NavBar.jsx
@@ -3,7 +3,7 @@
 import { useRouter } from 'next/navigation'
 import { gsap } from 'gsap'
 import Link from 'next/link'
-import React, { useEffect, useRef, useState ,useLayoutEffect} from 'react'
+import React, { useEffect, useRef, useState, useLayoutEffect } from 'react'
 
 
 const NavBar = () => {
@@ -14,123 +14,62 @@ const NavBar = () => {
   useLayoutEffect(() => {
     let tl = gsap.timeline();
     let ctx = gsap.context(() => {
-    tl.from(".logo, .menu, .menu1, .menu2, .menu3,.menu4,.btn",{
-            opacity: 0,
-            y: -20,
-            duration:1,
-            stagger:0.2,
-          });
+      tl.from(".logo, .menu, .menu1, .menu2, .menu3,.menu4,.btn", {
+        opacity: 0,
+        y: -20,
+        duration: 1,
+        stagger: 0.2,
+      });
     }, app);
-    
+
     return () => ctx.revert();
   }, []);
-  
+
 
 
   return (
-   
 
 
 
-      <header ref={app} className='mx-auto '  >
-        {/* <nav className="flex flex-wrap items-center justify-between bg-gray-900 m-auto  py-1 md:py-0.5 px-10 text-lg text-white  ">
+
+    <header ref={app} className='mx-auto ' >
+
+      <nav className="flex flex-wrap items-center justify-between bg-gray-900 m-auto   md:py-2 mx-10 px-2 text-lg text-white lg:rounded-xl content-center   font-bold border-b-2 ">
         <img className='w-12' src='/11.png' />
-          <div className="block lg:hidden">
-            <button
-              className="flex items-center px-3 py-2 border rounded  hover:text-white hover:border-white"
-              onClick={() => toggleExpansion(!isExpanded)}
-            >
-              <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-            </button>
-          </div>
-
-          <div className={`${isExpanded ? `block` : `hidden`
-            } w-full block  lg:flex lg:items-center lg:w-auto`}>
-            
-              <ul
-                className="text-base text-white pt-4 lg:flex text-center md:justify-between tracking-wider md:pt-2 md:px-2 uppercase font-bold" >
-                <Link  onClick={() => toggleExpansion(!isExpanded)}  className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/"> Home</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/work">Work</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/about"> About us</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/service">Service</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/contact">Contact Us</Link>
-              </ul>
-            
-          </div>
-
-     
-          <button className=" hidden ml-8 whitespace-nowrap lg:inline-flex items-center justify-center px-8 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-500  hover:text-blue-500 hover:bg-white "
-            type="button" onClick={() => router.push('/contact')}>
-            Get Quote
+        <div className="block lg:hidden">
+          <button
+            className="flex items-center px-3 py-2 border rounded  hover:text-white hover:border-white"
+            onClick={() => toggleExpansion(!isExpanded)}
+          >
+            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
           </button>
-
-        </nav> */}
-
-<<<<<<< HEAD
-
-        <nav className="flex flex-wrap items-center justify-between bg-gray-900 m-auto   md:py-2 mx-10 px-2 text-lg text-white lg:rounded-xl content-center   font-bold border-b-2 ">
-        <img className='w-12' src='/11.png' />
-          <div className="block lg:hidden">
-            <button
-              className="flex items-center px-3 py-2 border rounded  hover:text-white hover:border-white"
-              onClick={() => toggleExpansion(!isExpanded)}
-            >
-              <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-            </button>
-          </div>
-
-          <div className={`${isExpanded ? `block` : `hidden`
-            } w-full block  lg:flex lg:items-center lg:w-auto`}>
-            
-              <ul
-                className="text-base text-white pt-4 lg:flex text-center gap-3 md:justify-between tracking-wider md:pt-2 md:px-2 uppercase font-bold" >
-                <Link  onClick={() => toggleExpansion(!isExpanded)}  className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/"> Home</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/work">Work</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/about"> About us</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/service">Service</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/contact">Contact Us</Link>
-              </ul>
-            
-          </div>
-
-     
-          <button className=" hidden ml-8 whitespace-nowrap lg:inline-flex items-center justify-center px-8 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-500  hover:text-blue-500 hover:bg-white "
-            type="button" onClick={() => router.push('/contact')}>
-            Get Quote
-          </button>
-
-        </nav>
-        {/* <nav  className='px-6   text-white  flex items-center justify-between content-center h-16  font-bold border-b-2 ' >
-=======
-        <nav  className='px-6   text-white  md:flex items-center justify-between content-center h-16  font-bold border-b-2 ' >
->>>>>>> origin/main
-          <div className='logo font-bold tracking-wide text-lg '>WEBGTARIO</div>
-          <div className="block md:hidden">
-            <button
-              className="flex items-center px-3 py-2 border rounded  hover:text-white hover:border-white"
-              onClick={() => toggleExpansion(!isExpanded)}
-            >
-              <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-            </button>
-          </div>
-        <div className= {`${isExpanded ? `block` : `hidden`
-            } w-full block  lg:flex lg:items-center lg:w-auto`}>
-          <ul className='flex gap-4 font-bold uppercase tracking-wide'> 
-          <Link  onClick={() => toggleExpansion(!isExpanded)}  className="menu md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/"> Home</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className=" menu1 md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/work">Work</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className="menu2 md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/about"> About us</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className=" menu3 md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/service">Service</Link>
-                <Link onClick={() => toggleExpansion(!isExpanded)} className="menu4 md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/contact">Contact Us</Link>
-          </ul>
         </div>
 
-        <button className='btn lg:block hidden bg-white text-gray-950 px-4 py-2  rounded-md'> Get Quote</button>
+        <div className={`${isExpanded ? `block` : `hidden`
+          } w-full block  lg:flex lg:items-center lg:w-auto`}>
 
-        </nav> */}
-      </header>
+          <ul
+            className="text-base text-white pt-4 lg:flex text-center gap-3 md:justify-between tracking-wider md:pt-2 md:px-2 uppercase font-bold" >
+            <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/"> Home</Link>
+            <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/work">Work</Link>
+            <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/about"> About us</Link>
+            <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/service">Service</Link>
+            <Link onClick={() => toggleExpansion(!isExpanded)} className="md:p-2 block  hover:bg-white  hover:text-gray-900  rounded-lg" href="/contact">Contact Us</Link>
+          </ul>
+
+        </div>
 
 
-  
+        <button className=" hidden ml-8 whitespace-nowrap lg:inline-flex items-center justify-center px-8 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-500  hover:text-blue-500 hover:bg-white "
+          type="button" onClick={() => router.push('/contact')}>
+          Get Quote
+        </button>
+
+      </nav>
+    </header>
+
+
+
   )
 }
 
